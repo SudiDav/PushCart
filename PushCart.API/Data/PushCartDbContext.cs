6310a066a980c6a435f7ab7ef81a3e6a35ce2062
SudiDav <sudisimbadav@gmail.com>
2022-10-16 15:42:22 +0200

    first commit

---
+++ PushCart.API/Data/PushCartDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PushCart.API.Data;
+
+public class PushCartDbContext: DbContext
+{
+	public PushCartDbContext(DbContextOptions<PushCartDbContext> options): base(options)
+	{
+
+	}
+
+	public DbSet<Cart> Cart { get; set; }
+}
---
+++ PushCart.API/Models/Cart.cs
@@ -0,0 +1,9 @@
+﻿namespace PushCart.API.Models;
+
+public class Cart
+{
+    public int Id { get; set; }
+    public string? ItemName { get; set; }
+    public double Quantity { get; set; }
+    public bool IsPickedUp { get; set; }
+}
